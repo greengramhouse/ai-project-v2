7a9df2fac998b2c6dfa8190a29d8ce546b204738
waroon <waroon01@gmail.com>
2026-05-03 12:01:57 +0700

    start d3

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,20 +1,21 @@
+
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-if (!getApps().length) {
-  if (!process.env.FIREBASE_PRIVATE_KEY) {
-    throw new Error("Missing FIREBASE_PRIVATE_KEY");
+function initAdmin() {
+  if (!getApps().length) {
+    const serviceAccount = {
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    };
+
+    initializeApp({
+      credential: cert(serviceAccount as any),
+    });
   }
-
-  initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    } as any),
-  });
-
-  console.log("🔥 Firebase Admin Initialized");
 }
+
+initAdmin();
 
 export const firebaseAdmin = getFirestore();
