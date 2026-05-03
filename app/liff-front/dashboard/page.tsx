e1ea41565acc2521ec8d21c34cf5f62b2e2371a6
waroon <waroon01@gmail.com>
2026-05-03 20:32:26 +0700

    bookid

--- app/liff-front/dashboard/page.tsx
+++ app/liff-front/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { getUserProfiles } from "./actions";
 import UserManagementClient from "./UserManagementClient";
 
-export const dynamic = "force-dynamic";
 
 export default async function UserManagementPage() {
   const users = await getUserProfiles();
