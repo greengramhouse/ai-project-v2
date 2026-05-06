f168b72a0afb67f6baa48d9b38f9feef3b1df875
waroon <waroon01@gmail.com>
2026-05-06 20:12:03 +0700

    update for download data2

--- app/liff-front/page.tsx
+++ app/liff-front/page.tsx
@@ -3,12 +3,10 @@ import { getPublicEvents } from "@/lib/getEventData";
 import { getPublicNews } from "@/lib/getNewsData";
 import { fetchAllAlbumsInMainFolder } from "@/lib/getImageCloudinary";
 import { getUrlForDownload } from "@/lib/getUrlForDownload";
-import { connection } from "next/server";
 
 const GALLERY_FOLDER = "samples"; // ← ปรับให้ตรงกับชื่อโฟลเดอร์ใน Cloudinary
 
 export default async function LiffModernHomePage() {
-  await connection();
   const [events, news, albums, downloads] = await Promise.all([
     getPublicEvents(),
     getPublicNews(),
--- lib/getUrlForDownload.ts
+++ lib/getUrlForDownload.ts
@@ -4,18 +4,19 @@ import { connection } from "next/server";
 
 export async function getUrlForDownload() {
     await connection();
-    try{
-       const snapshot = await firebaseAdmin.collection('url_download').get();
+    try {
+        const snapshot = await firebaseAdmin.collection('url_download').get();
 
-       const url_download = snapshot.docs.map(doc => ({
+        const url_download = snapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data(),
-            createdAt: doc.data().createdAt?.toDate().toISOString() || null
+            createdAt: doc.data().createdAt?.toDate().toISOString() || null,
+            updatedAt: doc.data().updatedAt?.toDate().toISOString() || null
         }));
 
         return url_download;
-       
-    }catch(error){
+
+    } catch (error) {
         console.error("Error", error);
         return [];
     }
