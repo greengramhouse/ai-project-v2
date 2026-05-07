e506d8abc157067f79949fe55b11fbebc7e30218
waroon <waroon01@gmail.com>
2026-05-08 00:53:47 +0700

    test2

--- app/admin/table-user/page.tsx
+++ app/admin/table-user/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import UserTableData from "./UserTableData";
 import TableSkeleton from "./TableSkeleton";
 
+
 export default function PageTableUser() {
     return (
         <div className="p-4">
